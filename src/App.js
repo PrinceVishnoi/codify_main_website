0d5fe2ffa56875eae89eb349991db96e85993239
Prakhar Gupta <prakharg658@gmail.com>
2023-04-24 00:41:23 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Header from './component/Header'
-import Home from './component/HomePage/Home'
+// import Home from './component/HomePage/Home'
 import { ChakraProvider } from '@chakra-ui/react'
+import Service from "../src/component/ServicePage/Service"
 
 export default function App() {
   return (
     <>
       <ChakraProvider>
       <Header/>
-      <Home/>   
+     <Service/>  
        </ChakraProvider>
       </>
   
